5d59d3b26180e5425965c1722c6207729cbf92e7
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-02-16 16:32:29 -0300

    correção

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 class Database
 {
     public static function connect(){
-        $connect = new PDO('mysql:host=localhost;dbname=Revista', "root", "sua_senha");
+        $connect = new PDO('mysql:host=localhost;dbname=Revista', "root", "");
 
         return $connect;
     }
