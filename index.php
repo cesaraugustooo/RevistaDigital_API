d6d5295c9c77e78af5dc8c580c34705b2a73c31e
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-02-16 20:18:15 -0300

    Rota de posts

--- index.php
+++ index.php
@@ -13,4 +13,7 @@ switch(true){
     case $rota == '/categorias' or preg_match("#/categorias/(\d+)#", $rota , $array):
         require 'routes/categorias.php';
         break;
+    case $rota == '/posts' or preg_match("#/posts/(\d+)#", $rota, $array):
+        require 'routes/posts.php';
+        break;
 }
