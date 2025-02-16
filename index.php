6c678f3cee3da709bca428a593ac559f5858ce98
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-02-16 16:30:48 -0300

    caso de rotas

--- index.php
+++ index.php
@@ -3,4 +3,11 @@ require_once 'database.php';
 
 $url = $_SERVER['REQUEST_URI'];
 
-Database::connect();
+$rota = str_replace('/RevistaDigital_API', '' , $url );
+
+switch(true){
+    
+    case $rota == '/users' or preg_match("#/users/(\d+)#" , $rota , $array):
+        require 'routes/users.php';
+        break;
+}
