3313ab3f7414f2864321672ba1c3c79c4ba51758
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-02-16 20:18:37 -0300

    Novos metodos HTTP

--- controllers/posts.php
+++ controllers/posts.php
@@ -7,4 +7,16 @@ class Controller_Posts
         $json = json_decode(file_get_contents('php://input'),true);
         Postagens::POST($json);
     }
+    public static function get(){
+        header('Content-Type: application//json');
+        http_response_code(200);
+        echo json_encode(Postagens::get());
+    }
+    public static function delete($id){
+        Postagens::delete($id);
+    }
+    public static function update($id){
+        $json = json_decode(file_get_contents('php://input'),true);
+        Postagens::update($json,$id);
+    }
 }
