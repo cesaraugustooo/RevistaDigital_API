b181f49f8c4d4f1c5ff285041c54296ee4021071
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-02-16 20:19:23 -0300

    Rota de novos metodos HTTP

--- routes/posts.php
+++ routes/posts.php
@@ -1,8 +1,23 @@
 <?php
 require_once 'controllers/posts.php';
-
+require_once 'index.php';
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch($method){
-    
+    case 'POST':
+        Controller_Posts::POST();
+        break;
+    case 'GET':
+        Controller_Posts::get();
+        break;
+    case 'DELETE':
+        if(preg_match("#/posts/(\d+)#", $rota , $array)){
+            Controller_Posts::delete($array[1]);
+            break;
+        }
+    case 'PUT':
+        if(preg_match("#/posts/(\d+)#", $rota , $array)){
+            Controller_Posts::update($array[1]);
+            break;
+        }
 }
