481847130b41b151897baa25cae767a22176615c
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-02-18 16:49:09 -0300

    CORS adicionado

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'database.php';
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 $url = $_SERVER['REQUEST_URI'];
 
 $rota = str_replace('/RevistaDigital_API', '' , $url );
