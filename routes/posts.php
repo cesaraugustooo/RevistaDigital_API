4e3e0005b182173c1bd5d39e153b5e32eb339dda
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-03-02 23:22:55 -0300

    api-v2

--- routes/posts.php
+++ routes/posts.php
@@ -1,6 +1,14 @@
 <?php
-require_once 'controllers/posts.php';
-require_once 'index.php';
+require_once 'C:\xampp\htdocs\RevistaDigital_API\controllers\posts.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+$url = $_SERVER['REQUEST_URI'];
+
+$rota = str_replace('/RevistaDigital_API', '' , $url );
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch($method){
