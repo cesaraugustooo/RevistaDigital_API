c9195130a9a80f55978c9c1edf475c859834c1d8
CESAR AUGUSTO ESQUIÇACTO FERREIRA <cesar.ferreira@portalsesisp.org.br>
2025-04-04 15:41:09 -0300

    dding firebase and vendor

---
+++ models/login.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'c:/xampp/htdocs/RevistaDigital_API/json_request/json_sucess_error.php';
+require_once 'c:/xampp/htdocs/RevistaDigital_API/database.php';
+require_once '../vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class Login
+{
+    public static function login(){
+
+        $json = json_decode(file_get_contents('PHP://input'),true);
+        $db = Database::connect();
+        $sql = $db->prepare('SELECT id_usuario,user_usuario,nivel FROM usuarios WHERE user_usuario = :user AND senha_usuario = :senha');
+        $sql->bindValue(':user',$json['user_usuario']);
+        $sql->bindValue(':senha',$json['senha_usuario']);
+        $sql->execute();
+
+        $data = $sql->fetch(PDO::FETCH_ASSOC);
+
+        echo json_encode($data);
+
+        $key = 'a1ww2ei9';
+        $payload = [
+            'id' => $data['id_usuario'],
+            'user' => $data['user_usuario'],
+            'nivel' => $data['nivel']
+        ];
+
+        $jwt = JWT::encode($payload,$key,'HS256');
+        $decoded = JWT::decode($jwt,new Key($key,'HS256'));
+        echo $decoded;
+        
+    }
+}
--- routes/login.php
+++ routes/login.php
@@ -16,17 +16,8 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch($method){
 
     case 'POST':
-        UserController::postUser();
         if($rota == '/login'){
-            Login::login(json_decode(file_get_contents('PHP://input'),true));
+            Login::login();
         }
         break;
-    case 'GET':
-        UserController::getUsers();
-        
-        break;
-    case 'PUT':
-        if(preg_match("#/users/(\d+)#" , $rota , $array)){
-            UserController::updateUser($array[1]);
-        }
 }
