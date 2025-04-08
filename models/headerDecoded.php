315476ca306286dbf0a6993c1ec89456902d349f
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-04-08 12:28:00 -0300

    Classe para capturar token

---
+++ models/headerDecoded.php
@@ -0,0 +1,20 @@
+<?php
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+require_once '../vendor/autoload.php';
+
+class getToken
+{
+    public static function getToken($key){
+        $headers = getallheaders();
+
+        $authHeader = $headers['Authorization'];
+        if($authHeader){
+            $token = str_replace('Bearer ', '', $authHeader);
+            $decoded = JWT::decode($token,new Key($key, 'HS256'));
+
+            return $decoded;
+        }
+    }
+}
