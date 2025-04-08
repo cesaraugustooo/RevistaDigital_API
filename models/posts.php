f84a12e181fbe1137658ae7ceb9818e306500233
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-04-08 12:28:52 -0300

    update

--- models/posts.php
+++ models/posts.php
@@ -1,31 +1,37 @@
 <?php
 require_once 'C:\xampp\htdocs\RevistaDigital_API\database.php';
 require_once 'C:\xampp\htdocs\RevistaDigital_API\json_request\json_sucess_error.php';
+require_once 'C:/xampp/htdocs/RevistaDigital_API/models/headerDecoded.php';
+header('Content-Type: application/json');
 
 class Postagens
 {
     public static function POST($json){
-       
-        $db = Database::connect();
-        $sql = $db->prepare("INSERT INTO posts VALUES(null,:titulo,:foto,:descricao,:data, :usuario, :categoria, :status, :sub , :subf,:subtext)"); 
-        $sql->bindValue(':titulo',$json['titulo_post']);
-        $sql->bindValue(':foto',$json['foto_post']);
-        $sql->bindValue(':descricao',$json['descricao_post']);
-        $sql->bindValue(':data',date('Y-m-d H:i:s'));
-        $sql->bindValue(':usuario',$json['usuarios_id_usuario']);
-        $sql->bindValue(':categoria',$json['categorias_id_categoria']);
-        $sql->bindValue(':status',$json['status_post']);
-        $sql->bindValue(':sub',$json['sub_titulo_post']);
-        $sql->bindValue(':subf',$json['sub_foto_post']);
-        $sql->bindValue(':subtext',$json['sub_descricao_post']);
+        $key = '12345';
 
+        $payload = getToken::getToken($key);
+        if($payload->nivel == 'Admin' or $payload->nivel == 'Professor'){
+            $db = Database::connect();
+            $sql = $db->prepare("INSERT INTO posts VALUES(null,:titulo,:foto,:descricao,:data, :usuario, :categoria, :status, :sub , :subf,:subtext)"); 
+            $sql->bindValue(':titulo',$json['titulo_post']);
+            $sql->bindValue(':foto',$json['foto_post']);
+            $sql->bindValue(':descricao',$json['descricao_post']);
+            $sql->bindValue(':data',date('Y-m-d H:i:s'));
+            $sql->bindValue(':usuario',$json['usuarios_id_usuario']);
+            $sql->bindValue(':categoria',$json['categorias_id_categoria']);
+            $sql->bindValue(':status',$json['status_post']);
+            $sql->bindValue(':sub',$json['sub_titulo_post']);
+            $sql->bindValue(':subf',$json['sub_foto_post']);
+            $sql->bindValue(':subtext',$json['sub_descricao_post']);
 
+            $sql->execute();
 
+            successJson($sql);
+        }else{
+            http_response_code(401);
+            echo json_encode(["message"=>"Usuario sem permissão"]);
 
-
-        $sql->execute();
-        
-        successJson($sql);
+        }
     }
     public static function get(){
         $db = Database::connect();
@@ -35,28 +41,48 @@ class Postagens
         return $sql->fetchAll(PDO::FETCH_ASSOC);
     }
     public static function update($json,$id){
-        $db = Database::connect();
-        $sql = $db->prepare("UPDATE posts SET titulo_post = :titulo, foto_post = :foto, descricao_post = :descricao, data_criacao_post = :data, categorias_id_categoria = :categoria, status_post = :status WHERE id_post = :id");
-        $sql->bindValue(':titulo',$json['titulo_post']);
-        $sql->bindValue(':foto',$json['foto_post']);
-        $sql->bindValue(':descricao',$json['descricao_post']);
-        $sql->bindValue(':data',$json['data_criacao_post']);
-        $sql->bindValue(':categoria',$json['categorias_id_categoria']);
-        $sql->bindValue(':status',$json['status_post']);
+        $key = '12345';
 
-        $sql->bindValue(':id', $id , PDO::PARAM_INT);
+        $payload = getToken::getToken($key);
+        if($payload->nivel == 'Admin' or $payload->nivel == 'Professor'){
 
-        $sql->execute();
-        successJson($sql);
+            $db = Database::connect();
+            $sql = $db->prepare("UPDATE posts SET titulo_post = :titulo, foto_post = :foto, descricao_post = :descricao, data_criacao_post = :data, categorias_id_categoria = :categoria, status_post = :status WHERE id_post = :id");
+            $sql->bindValue(':titulo',$json['titulo_post']);
+            $sql->bindValue(':foto',$json['foto_post']);
+            $sql->bindValue(':descricao',$json['descricao_post']);
+            $sql->bindValue(':data',$json['data_criacao_post']);
+            $sql->bindValue(':categoria',$json['categorias_id_categoria']);
+            $sql->bindValue(':status',$json['status_post']);
+
+            $sql->bindValue(':id', $id , PDO::PARAM_INT);
+
+            $sql->execute();
+            successJson($sql);
+        }else{
+            http_response_code(401);
+            echo json_encode(["message"=>"Usuario sem permissão"]);
+
+        }
     }
     public static function delete($id){
-        $db = Database::connect();
-        $sql = $db->prepare("DELETE FROM posts WHERE id_post = :id");
-        $sql->bindValue(':id', $id , PDO::PARAM_INT);
+        $key = '12345';
 
-        $sql->execute();
+        $payload = getToken::getToken($key);
 
-        successJson($sql);
+        if($payload->nivel == 'Admin' or $payload->nivel == 'Professor'){
+            $db = Database::connect();
+            $sql = $db->prepare("DELETE FROM posts WHERE id_post = :id");
+            $sql->bindValue(':id', $id , PDO::PARAM_INT);
+
+            $sql->execute();
+
+            successJson($sql);
+        }else{
+            http_response_code(401);
+            echo json_encode(["message"=>"Usuario sem permissão"]);
+
+        }
     }
     public static function getID($id){
         $db = Database::connect();
@@ -75,20 +101,41 @@ class Postagens
         return $sql->fetchAll(PDO::FETCH_ASSOC);
     }
     public static function getPostNull(){
-        $db = Database::connect();
-        $sql = $db->prepare("SELECT * FROM posts WHERE status_post = 0");
-        $sql->execute();
+        $key = '12345';
 
-        return $sql->fetchAll(PDO::FETCH_ASSOC);
+        $payload = getToken::getToken($key);   
+        if($payload->nivel == 'Admin' or $payload->nivel == 'Professor'){
+            $db = Database::connect(); 
+            $sql = $db->prepare("SELECT * FROM posts WHERE status_post = 0");
+            $sql->execute();
+    
+            return $sql->fetchAll(PDO::FETCH_ASSOC);
+        }else{
+            http_response_code(401);
+            echo json_encode(["message"=>"Usuario sem permissão"]);
+
+        }
+
+       
     }
     public static function updateStatus($json,$id){
-        $db = Database::connect();
-        $sql = $db->prepare("UPDATE posts SET  status_post = :status  WHERE id_post = :id");
-        $sql->bindValue(':status',$json['status_post']);
-        $sql->bindValue(':id', $id , PDO::PARAM_INT);
+        $key = '12345';
 
-        $sql->execute();
-        successJson($sql);
+        $payload = getToken::getToken($key);
+        if($payload->nivel == 'Admin' or $payload->nivel == 'Professor'){
+            $db = Database::connect();
+            $sql = $db->prepare("UPDATE posts SET  status_post = :status  WHERE id_post = :id");
+            $sql->bindValue(':status',$json['status_post']);
+            $sql->bindValue(':id', $id , PDO::PARAM_INT);
+
+            $sql->execute();
+            successJson($sql);
+        }else{
+            http_response_code(401);
+            echo json_encode(["message"=>"Usuario sem permissão"]);
+
+        }
+
     }
     
 }
